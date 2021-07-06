19a77d5b0fd22977071acad6078ad82c86cf9870
Gianluca Bergamini <gianlu.bergamini@gmail.com>
2021-07-06 17:58:52 +0200

    Changed skybox image, deleted not necessary files

--- lib/shaders/skyvs.glsl
+++ lib/shaders/skyvs.glsl
@@ -6,7 +6,7 @@ out vec3 sampleDir;
 
 void main() {
     // Multiply the position by the matrix.
-    gl_Position = vec4(in_position,1.0);
+    gl_Position = vec4(in_position, 1.0);
 
     // Pass a normal. Since the positions are
     // centered around the origin we can just
